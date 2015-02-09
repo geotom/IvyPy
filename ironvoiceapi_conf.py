c1fdf89ea6454b902a412966d86850724b3dfb28
geotom <tarnold@walkercivil.com>
2015-02-09 16:49:05 -0600

    Untrack local conf file

--- ironvoiceapi_conf.py
+++
@@ -1,6 +0,0 @@
-# If this file can be found by the "import" statement in Python,
-# then the ironvoiceapi.py module will use the host name and key
-# provided here unless you pass them in upon object creation
-
-host = # Example: 'https://pop3.ironvoice.com'
-key = # Your API key
