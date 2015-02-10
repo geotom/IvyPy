7fd05683e64d7a61ae788b55a1ca9182a4ef8b94
geotom <tarnold@walkercivil.com>
2015-02-09 21:25:51 -0600

    Working commit

--- ironvoiceapi.py
+++ ironvoiceapi.py
@@ -2,6 +2,7 @@
 
 import urllib
 
+
 class ivapi:
     
     def __init__(self, host = None, key = None):
@@ -13,19 +14,33 @@ class ivapi:
             except:
                 raise Exception('Configuration file "ironvoiceapi_conf.py" not found.')
         try:
-            self.base_url = '{}/api/?key={}&'.format(host, key)
-            url_test = urllib.urlopen('{}method=get'.format(self.base_url))
+            self.base_url = '{}/api/?key={}'.format(host, key)
+            url_test = urllib.urlopen('{}&method=get'.format(self.base_url))
             okay = next(url_test)
         except:
             raise Exception('Invalid host and/or API key.')
-    
+
+    def _make_query(self, method, params = None):
+        query_url = '{}&method={}'.format(self.base_url, method)
+        if isinstance(params, dict):
+            for param in params.items():
+                if param[1]:
+                    query_url = '{}&{}={}'.format(query_url, param)
+                else:
+                    query_url = '{}&{}'.format(query_url, param[0])
+        return urllib.urlopen(query_url)
+
     def trace(self, host):
+        result = _make_query(
+                method = 'trace',
+                params = {'host': str(host)},
+        )
         hops = []
         return hops
 
     def peers(self):
-        peers_dict = {}
-        print next(urllib.urlopen('{}method=peers'.format(self.base_url)))
-        return peers_dict
+        results = self._make_query(method = 'peers')
+        for result in results:
+            print result
 
     
